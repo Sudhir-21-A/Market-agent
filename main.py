285e57b8afdf7e0a62ee12dfa705cdae8a7d1448
sa2106 <sudhira2106@gmail.com>
2026-07-17 00:50:06 +0530

    Added some spacing,margins

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QHBoxLayout,QLabel,QLineEdit,QPushButton
+from PySide6.QtWidgets import QMainWindow,QApplication,QWidget,QVBoxLayout,QHBoxLayout,QLabel,QLineEdit,QPushButton,QListWidget
 from PySide6.QtCore import Qt
 
 class MainWindow(QMainWindow):
@@ -11,25 +11,30 @@ class MainWindow(QMainWindow):
         main_layout.addWidget(UserWidget())
         main_layout.addWidget(WatchListWidget())
         main_layout.addWidget(RecentWidget())
+        main_layout.setContentsMargins(20,20,20,20)
+        main_layout.setSpacing(50)
 
 
 class UserWidget(QWidget):
     def __init__(self):
         super(). __init__()
         userLayout=QVBoxLayout(self)
-        userlabel=QLabel('Username ABCD')
-        userLayout.addWidget(userlabel)
+        self.userlabel=QLabel('Username ABCD')
+        userLayout.addWidget(self.userlabel)
         userLayout.addWidget(SearchBarWidget())
-
+        userLayout.setSpacing(40)
+        userLayout.setContentsMargins(0,0,0,0)
 
 class WatchListWidget(QWidget):
     def __init__(self):
         super(). __init__()
         watchlabel=QLabel('Current Watchlist:')
         watchlist_layout=QVBoxLayout(self)
-        wlabel1=QLabel('Nvidia  +3.111%')
+        watch_list=QListWidget()
+        watch_list.addItems(['Nvidia +3.167%','Microsoft +2.71%'])
         watchlist_layout.addWidget(watchlabel)
-        watchlist_layout.addWidget(wlabel1)
+        watchlist_layout.addWidget(watch_list)
+        watchlist_layout.setSpacing(50)
 
 
 
@@ -39,9 +44,11 @@ class RecentWidget(QWidget):
         super(). __init__()
         recentlabel=QLabel('Recent Changes:')
         recent_layout=QVBoxLayout(self)
-        rlabel1=QLabel('Nvidia  goes up by 5%....')
+        recent_list=QListWidget()
+        recent_list.addItems(['Nvidia goes up due to increased demand','Microsoft announces new windows features increasing value'])
         recent_layout.addWidget(recentlabel)
-        recent_layout.addWidget(rlabel1)
+        recent_layout.addWidget(recent_list)
+        recent_layout.setSpacing(50)
 
 
 
@@ -55,6 +62,8 @@ class SearchBarWidget(QWidget):
         search_button=QPushButton('SEARCH')
         search_bar_layout.addWidget(searchbox)
         search_bar_layout.addWidget(search_button)
+        search_bar_layout.setSpacing(0)
+        search_bar_layout.setContentsMargins(0,0,0,40)
 
 
 
